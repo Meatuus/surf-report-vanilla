a8f6b0cce65d117ca5de81b05efb3252e0db0fd4
Tyler <tcameronsmith@gmail.com>
2017-09-22 17:28:57 +1000

    add axios dependecy to make requests from front end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import conditions from './data/locations';
 import baseData from './data/baseData';
+import axios from 'axios';
+// var request = require('../request');
 
 // const surfForecast = document.querySelector('.surf-forecast');
 // const location = document.querySelector('.surf-conditions');
@@ -8,11 +10,33 @@ const swell = document.getElementsByClassName('swell');
 const wind = document.getElementsByClassName('wind');
 const temp = document.getElementsByClassName('temp');
 
-const conditionsOne = baseData;
-const conditionsTwo = baseData;
-const conditionsThree = baseData;
+let conditionsOne = baseData;
+let conditionsTwo = baseData;
+let conditionsThree = baseData;
 
+Promise.all([
+  axios.get("http://localhost:3000/locationOne"),
+  axios.get("http://localhost:3000/locationTwo"),
+  axios.get("http://localhost:3000/locationThree")
+]).then((res) => {
+  console.log("test");
+  console.log(res);
+})
 
+// loadCommentsFromServer() {
+//   Promise.all([
+//     axios.get(this.props.urlFirst),
+//     axios.get(this.props.urlSecond),
+//     axios.get(this.props.urlThird)
+//   ]).then((res) => {
+//     this.setState({
+//       conditionsOne: res[0].data,
+//       conditionsTwo: res[1].data,
+//       conditionsThree: res[2].data,
+//       loading: false,
+//     });
+//   });
+// }
 
 if (conditions.length > 0) {
   for (var i = 0; i < conditions.length; i++) {
