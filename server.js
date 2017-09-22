4b12f3485978d57821dcaab8b5b74ca203000f80
Tyler <tcameronsmith@gmail.com>
2017-09-22 17:28:28 +1000

    fix cors issue

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
-// // To prevent errors from Cross Origin Resource Sharing, we will set
-// // our headers to allow CORS with middleware like so:
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Credentials', 'true');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-//
-// //and remove cacheing so we get the most recent comments
-//   res.setHeader('Cache-Control', 'no-cache');
-//   next();
-// });
+// To prevent errors from Cross Origin Resource Sharing, we will set
+// our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+//and remove cacheing so we get the most recent comments
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
 
 var routes = require('./api/routes/vanillaSurfRoutes'); //importing route
 
